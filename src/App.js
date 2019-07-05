87e4de7ff123eac5de944630295c3e06e5b36d5d
nanexo <nanexxo@gmail.com>
2019-07-05 21:42:51 +0200

    introduced a dock layout, move foodtable to the bottom

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import './App.css';
 import MealContainer from './MealContainer';
 import FoodTable from './FoodTable';
+import DockContainer from './DockContainer';
 
 function App() {
 	return (
-		<div className="wrapper">
-			<div style={{flexGrow: 2}}>
-				<MealContainer />
-			</div>
-			<div className="vertical-line" />
-			<div style={{flexGrow: 1}}>
-				<FoodTable />
-			</div>
-		</div>
+		<DockContainer
+			center={<MealContainer />}
+			bottom={<FoodTable />}
+		/>
 	);
 }
 
---
+++ src/DockContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './DockContainer.css';
+
+function DockContainer(props) {
+
+	const prepareContent = (name) => {
+		return (!!props[name] ? <div className={name + '-content'}>{props[name]}</div> : null)
+	};
+
+	const content = ['top', 'left', 'center', 'right', 'bottom'].map(prepareContent);
+
+	return (
+		<div className="wrapper">
+			{content}
+		</div>
+	);
+}
+
+export default DockContainer;
