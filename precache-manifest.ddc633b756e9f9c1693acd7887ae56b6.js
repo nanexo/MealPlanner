675b3a2cedf1e9eb11ea9855d9330b9400444f99
nanexo <nanexo@users.noreply.github.com>
2019-07-17 18:56:58 +0200

    Updates

---
+++ precache-manifest.ddc633b756e9f9c1693acd7887ae56b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f5fc4ba6b45b5afaeb7233553f7b5d",
+    "url": "/MealPlanner/index.html"
+  },
+  {
+    "revision": "026650ae2c8876d5f030",
+    "url": "/MealPlanner/static/css/main.90be9660.chunk.css"
+  },
+  {
+    "revision": "b34c26e9695286e0fa19",
+    "url": "/MealPlanner/static/js/2.dbf2876a.chunk.js"
+  },
+  {
+    "revision": "026650ae2c8876d5f030",
+    "url": "/MealPlanner/static/js/main.38e2dfb7.chunk.js"
+  },
+  {
+    "revision": "7c9df26d9d95257cddc5",
+    "url": "/MealPlanner/static/js/runtime~main.c7153f9f.js"
+  },
+  {
+    "revision": "eec47024c0372b2325ad7f28d1734dd2",
+    "url": "/MealPlanner/static/media/arrow.eec47024.svg"
+  }
+]);
